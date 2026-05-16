78b43c7f6eb56e5def02b84e2a84087a4b025bea
ashishgupta09 <ashishgupta95652@gmail.com>
2026-05-16 15:51:45 +0530

    remove the extra code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,15 +7,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './services/interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    )
+    provideHttpClient()
   ]
 };
